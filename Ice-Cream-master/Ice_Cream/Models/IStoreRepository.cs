67e88c1adcca8ce33eb97ee6c6d612fd91f9035a
sonc3k562001 <54662662+sonc3k562001@users.noreply.github.com>
2020-12-08 14:32:07 +0700

    ok

---
+++ Ice-Cream-master/Ice_Cream/Models/IStoreRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ice_Cream.Models
+{
+    public interface IStoreRepository
+    {
+        IQueryable<Product> Products { get; }
+        IQueryable<Customer> Customers { get; }
+    }
+}
